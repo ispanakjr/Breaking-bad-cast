7af2b56bc93747d24bcda8b5e20d7e93b7bcbe80
ispanakjr <77546679+ispanakjr@users.noreply.github.com>
2021-05-03 22:19:06 +0300

    Updates

---
+++ precache-manifest.6df62a14cf77bc0d8c534bf7c1165b08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d796cf82740c765fc09",
+    "url": "/github/io/Breaking-bad-cast/static/css/main.efca58f7.chunk.css"
+  },
+  {
+    "revision": "4d796cf82740c765fc09",
+    "url": "/github/io/Breaking-bad-cast/static/js/main.263ab8e4.chunk.js"
+  },
+  {
+    "revision": "28c4b1b9ec6e764d0371",
+    "url": "/github/io/Breaking-bad-cast/static/js/runtime~main.1eaf7f3b.js"
+  },
+  {
+    "revision": "3f9057110d8290f32adf",
+    "url": "/github/io/Breaking-bad-cast/static/js/2.804ffc6f.chunk.js"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/github/io/Breaking-bad-cast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/github/io/Breaking-bad-cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "1e98cb07e4f0148f7f52b24cdfadfb9e",
+    "url": "/github/io/Breaking-bad-cast/index.html"
+  }
+];
